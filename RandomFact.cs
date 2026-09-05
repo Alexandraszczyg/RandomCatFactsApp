fc308566cda1393fb2a3b29b94c8a00c3e310e77
Alexandraszczyg <alexandraszczyg@gmail.com>
2026-09-05 22:05:22 +0200

    RandomFact class - data receiving object setup

---
+++ RandomFact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCatFactsApp
+{
+    public class RandomFact
+    {
+        public required string Fact { get; set; }
+        public required int Length { get; set; }
+    }
+}
+
