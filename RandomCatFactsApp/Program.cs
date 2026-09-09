1d828a8894334d04dbb34c8a527d9b993b8b2953
Alexandraszczyg <alexandraszczyg@gmail.com>
2026-09-09 16:27:38 +0200

    Builder and app set up configured

--- RandomCatFactsApp/Program.cs
+++ RandomCatFactsApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHttpClient();
+
+var host = builder.Build();
+await host.RunAsync();
