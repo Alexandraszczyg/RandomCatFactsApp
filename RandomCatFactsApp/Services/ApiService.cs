df2277d69731447ac9b40df76659c2460bddba51
Alexandraszczyg <alexandraszczyg@gmail.com>
2026-09-09 16:26:50 +0200

    Api service dependency injection - httpclient added

--- RandomCatFactsApp/Services/ApiService.cs
+++ RandomCatFactsApp/Services/ApiService.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace RandomCatFactsApp.Services
 {
-    public class ApiService
+    public class ApiService : IApiService
     {
+        private readonly HttpClient _httpClient;
 
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            
+        }
+
+        public Task<string> GetRandomFactAsync()
+        {
+            
+        }
     }
 }
+
