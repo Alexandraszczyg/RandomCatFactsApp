4238364e7df8937bb22e415d6773c97c83e559f9
Alexandraszczyg <alexandraszczyg@gmail.com>
2026-09-10 17:11:41 +0200

    registration of api service and filetxtsaver, and call of mehtod savetofileasync

--- RandomCatFactsApp/Controller/FileTxtSaver.cs
+++ RandomCatFactsApp/Controller/FileTxtSaver.cs
@@ -15,19 +15,27 @@ namespace RandomCatFactsApp.Controller
         private readonly IConfiguration _configuration;
         private readonly IApiService _apiService;
         private readonly string _filePath;
+        private readonly int _numberOfRequests;
 
         public FileTxtSaver(IConfiguration configuration, IApiService apiService)
         {
             _configuration = configuration;
             _apiService = apiService;
             _filePath = _configuration.GetValue<string>("FileSettings:FilePath")!;
+            _numberOfRequests = _configuration.GetValue<int>("FileSettings:NumberOfRequests"); 
         }
 
 
-        public async Task<RandomFact> SaveToFileAsync()
+        public async Task SaveToFileAsync()
         {
-            var randomFact = await _apiService.GetRandomFactAsync();
+            
 
+            for ( int i=0; i < _numberOfRequests; i++)
+            {
+                var randomFact = await _apiService.GetRandomFactAsync();
+               await File.AppendAllTextAsync(_filePath, randomFact.Fact + ";" + randomFact.Length + Environment.NewLine);
+            }
+            
         }
        
     }
--- RandomCatFactsApp/Program.cs
+++ RandomCatFactsApp/Program.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RandomCatFactsApp.Controller;
+using RandomCatFactsApp.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Configuration.AddJsonFile("Configuration/appsettings.json");
-builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<IApiService,ApiService>();
+builder.Services.AddScoped<FileTxtSaver>();
 
 var host = builder.Build();
-await host.RunAsync();
+var fileTxtSaver = host.Services.GetRequiredService<FileTxtSaver>();
+await fileTxtSaver.SaveToFileAsync();
