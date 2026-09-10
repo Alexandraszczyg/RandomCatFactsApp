0c646e2857c9825f3ac0f50156bd43a834f16761
Alexandraszczyg <alexandraszczyg@gmail.com>
2026-09-10 12:26:14 +0200

    Added json file to the builder

--- RandomCatFactsApp/Program.cs
+++ RandomCatFactsApp/Program.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Configuration.AddJsonFile("Configuration/appsettings.json");
 builder.Services.AddHttpClient();
 
 var host = builder.Build();
