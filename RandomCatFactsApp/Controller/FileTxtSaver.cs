7e47bb9a3c48a83a2fb6cb418b2f5c5fc42bb5cc
Alexandraszczyg <alexandraszczyg@gmail.com>
2026-09-10 15:58:48 +0200

    Constructor and method in FileTxtSaver set up

--- RandomCatFactsApp/Controller/FileTxtSaver.cs
+++ RandomCatFactsApp/Controller/FileTxtSaver.cs
@@ -5,12 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Http;
+using RandomCatFactsApp.Models;
+using RandomCatFactsApp.Services;
 
 namespace RandomCatFactsApp.Controller
 {
     internal class FileTxtSaver
     {
         private readonly IConfiguration _configuration;
+        private readonly IApiService _apiService;
         private readonly string _filePath;
+
+        public FileTxtSaver(IConfiguration configuration, IApiService apiService)
+        {
+            _configuration = configuration;
+            _apiService = apiService;
+            _filePath = _configuration.GetValue<string>("FileSettings:FilePath")!;
+        }
+
+
+        public async Task<RandomFact> SaveToFileAsync()
+        {
+            var randomFact = await _apiService.GetRandomFactAsync();
+
+        }
+       
     }
 }
