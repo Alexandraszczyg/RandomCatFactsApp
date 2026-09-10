a5b1afeaa718d837ef3b65b65a391979ed11b8fd
Alexandraszczyg <alexandraszczyg@gmail.com>
2026-09-10 15:05:24 +0200

    updated method from string returntype to object of random fact

--- RandomCatFactsApp/Services/IApiService.cs
+++ RandomCatFactsApp/Services/IApiService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RandomCatFactsApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace RandomCatFactsApp.Services
 {
     public interface IApiService
     {
-        Task<string> GetRandomFactAsync();
+        Task<RandomFact> GetRandomFactAsync();
     }
 }
