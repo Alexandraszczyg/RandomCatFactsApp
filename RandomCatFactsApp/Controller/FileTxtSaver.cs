99fc5b0cb66433193c0b73bda921889f3baa727e
Alexandraszczyg <alexandraszczyg@gmail.com>
2026-09-10 15:37:26 +0200

    Zmiana nazwy Fact Controller na FileTxtSaver oraz implementacja private fields

--- RandomCatFactsApp/Controller/FileTxtSaver.cs
+++ RandomCatFactsApp/Controller/FileTxtSaver.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Http;
 
 namespace RandomCatFactsApp.Controller
 {
-    internal class FactController
+    internal class FileTxtSaver
     {
-       
+        private readonly IConfiguration _configuration;
+        private readonly string _filePath;
     }
 }
